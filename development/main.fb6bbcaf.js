84ae4bda87857d71afb52c0647e1006822a969b7
‘Juliano <julianoperins@gmail.com>
2020-07-16 04:58:35 -0400

    Worked on the projects section

--- development/main.fb6bbcaf.js
+++ development/main.fb6bbcaf.js
@@ -118,11 +118,53 @@ parcelRequire = (function (modules, cache, entry, globalName) {
 
   return newRequire;
 })({"js/main.js":[function(require,module,exports) {
-var sayHello = function sayHello() {
-  console.log("HELOOO!!!");
-};
-
-sayHello();
+$(document).ready(function () {
+  var $btns = $(".project-area .button-group button");
+  $btns.click(function (e) {
+    $(".project-area .button-group button").removeClass("active");
+    e.target.classList.add("active");
+    var selector = $(e.target).attr("data-filter");
+    $(".project-area .grid").isotope({
+      filter: selector
+    });
+    return false;
+  });
+  $(".project-area .button-group #btn1").trigger("click");
+  $(".project-area .grid .test-popup-link").magnificPopup({
+    type: "image",
+    gallery: {
+      enabled: true
+    }
+  }); // Owl-carousel
+  // $(".site-main .about-area .owl-carousel").owlCarousel({
+  //   loop: true,
+  //   autoplay: true,
+  //   dots: true,
+  //   responsive: {
+  //     0: {
+  //       items: 1,
+  //     },
+  //     560: {
+  //       items: 2,
+  //     },
+  //   },
+  // });
+  // sticky navigation menu
+  // let nav_offset_top = $(".header_area").height() + 50;
+  // function navbarFixed() {
+  //   if ($(".header_area").length) {
+  //     $(window).scroll(function () {
+  //       let scroll = $(window).scrollTop();
+  //       if (scroll >= nav_offset_top) {
+  //         $(".header_area .main-menu").addClass("navbar_fixed");
+  //       } else {
+  //         $(".header_area .main-menu").removeClass("navbar_fixed");
+  //       }
+  //     });
+  //   }
+  // }
+  // navbarFixed();
+});
 },{}],"../../../../../usr/local/lib/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
